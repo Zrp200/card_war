7a2434625b92b9d87e94a0b32113a01646b867f5
Zachary Perlmutter <zrp200@gmail.com>
2014-09-02 18:26:04 -0700

    Update war_spec.rb

--- spec/bin/war_spec.rb
+++ spec/bin/war_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "war" do
 	describe "--help" do
-		subject {`war --help`}
+		subject {`bin/war --help`}
 		it "should return a help message" do
 			expect(subject).to match /version/
 		end
