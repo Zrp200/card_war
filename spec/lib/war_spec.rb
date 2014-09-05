32e01660fbdc4f12322e7797c4098666dbee5ba3
Zachary Perlmutter <zrp200@gmail.com>
2014-09-04 18:22:57 -0700

    Update war_spec.rb

--- spec/lib/war_spec.rb
+++ spec/lib/war_spec.rb
@@ -1,1 +1,15 @@
-
+require 'spec_helper'
+include CardDeck
+RSpec.describe Card do
+	describe "#value" do
+		number = (2..10).to_a.sample
+		context "when #{number}" do
+			subject {Card.new number}
+			it "should == {subject.num}" {expect(subject.value).to eq subject.num}
+		end
+		context "when Jack" do
+			subject {Card.new "Jack"}
+			it "should == 11" {expect(subject.value).to eq 11}
+		end
+	end
+end
