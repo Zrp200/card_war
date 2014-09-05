8c46849d107ba6f0dab6536bbbe31d26e5f34b01
Zachary Perlmutter <zrp200@gmail.com>
2014-09-04 18:28:49 -0700

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,6 +3,6 @@ namespace 'build' do
 		puts `gem build war.gemspec`
 		puts `git add war-0.0.0.gem`
 		puts `git commit war-0.0.0.gem -m "rake build:000"`
-		puts `git push`
+		puts `git push` 
 	end
 end
