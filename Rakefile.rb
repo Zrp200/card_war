0d8e9fba619524ee2b3dd23f00a9aaacadb23083
Zachary Perlmutter <zrp200@gmail.com>
2014-09-05 18:27:50 -0700

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,14 +1,14 @@
 namespace 'build' do
-	task '000'.to_sym do		`gem uninstall war`
+	task '000'.to_sym do
 		puts `git pull`
-		puts `gem build war.gemspec`
-		puts `gem install war-0.0.0.gem`
+		puts `gem build card_war.gemspec`
+		puts `gem install card_war-0.0.0.gem`
 		if ENV['PUSH_ALL']
 			`git add -A`
 			`git commit -m "rake build:000"`
 		else
-			puts `git add war-0.0.0.gem`
-			puts `git commit war-0.0.0.gem -m "rake build:000"`
+			puts `git add card_war-0.0.0.gem`
+			puts `git commit card_war-0.0.0.gem -m "rake build:000"`
 		end
 		puts `git push` 
 	end
