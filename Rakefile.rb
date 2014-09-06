33c88b4e0bcab944b61954f95e272215bf517de8
Zachary Perlmutter <zrp200@gmail.com>
2014-09-05 17:05:53 -0700

    rake build:000

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,8 +1,10 @@
 namespace 'build' do
 	task '000'.to_sym do
+		puts `git pull`
 		puts `gem build war.gemspec`
 		puts `git add war-0.0.0.gem`
 		puts `git commit war-0.0.0.gem -m "rake build:000"`
+		puts `git add -A && git commit -m "rake build:000"` if ENV['PUSH_ALL']
 		puts `git push` 
 	end
 end
