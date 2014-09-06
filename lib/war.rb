dfd6165efafc819c1de83b453e7a193669ae2ef7
Zachary Perlmutter <zrp200@gmail.com>
2014-09-05 17:11:10 -0700

    rake build:000

--- lib/war.rb
+++ lib/war.rb
@@ -1,10 +1,9 @@
-# lib/war.rb
 require "card_deck"
 class CardDeck::Card
     # The card's value
         def value
             case @num
-                when 2..10 then @num
+                when 2..10 then @num.to_i
                 when "Jack" then 11
                 when "Queen" then 12
                 when "King" then 13
